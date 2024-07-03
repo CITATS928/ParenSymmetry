4b7d43aa1bcbba8b322dc9172ead812c198d81f4
CITATS928 <93238522+CITATS928@users.noreply.github.com>
2024-07-03 08:01:04 -0400

    Finished 7/3

--- src/main/java/ParenSymmetry.java
+++ src/main/java/ParenSymmetry.java
@@ -1,15 +1,42 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class ParenSymmetry {
 
 
     private Boolean isBalanced(String s) {
         // implement this method
-        return null;
+        int counterL=0;
+        int counterR=0;
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)=='('){
+                counterL++;
+            } else if (s.charAt(i)==')') {
+                counterR++;
+            }
+        }
+
+        if(counterR==counterL){
+            //System.out.println("L is "+counterL+"\nR is "+counterR);
+            return true;
+        }else{
+            //System.out.println("L is "+counterL+"\nR is "+counterR);
+            return false;
+        }
+
     }
 
-    private void checkFile(String filename) {
+    private void checkFile(String filename) throws FileNotFoundException {
         // open file named filename
+        Scanner fileIn = new Scanner(new File(filename));
 
+        while(fileIn.hasNext()){
+            String lineIn = fileIn.nextLine();
+            System.out.println(isBalanced(lineIn));//isBalanced(lineIn);
+        }
+
+        fileIn.close();
         // for each line in the file
             // read the line
             // print whether or not the line's parenthesis are balanced
@@ -17,8 +44,12 @@ public class ParenSymmetry {
         // CLOSE the file
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
         ParenSymmetry ps = new ParenSymmetry();
+
+        ps.checkFile("/Users/qian/Desktop/ZipCode/Java/ParenSymmetry/TestStrings0.txt");
+        System.out.println("==============");
+
 
         Boolean b0 = ps.isBalanced("()");
         printResult(b0, true);
